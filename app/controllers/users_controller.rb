db65ed0c7de03e070eaf5837fc4e2e0cd138ea26
Ammad <writer648@yahoo.com>
2017-10-18 18:33:47 -0500

    Added Delete functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,10 +54,14 @@ class UsersController < ApplicationController
   end
 
   def delete
+    @user = User.find(params[:id])
   end
 
   # no view so manually created
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to(users_path)
   end
 
   private
